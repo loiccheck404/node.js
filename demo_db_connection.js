8244658898ce168172a82491166650fd46bd0d80
Loic CHECK <ndzieloic334@gmail.com>
2025-04-11 16:22:36 +0100

    demo_db_connection.js

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "localhost:8080",
-  user: "loiccheck",
-  password: "Biyan4ever7#",
+  host: "localhost",
+  user: "localuser",
+  password: ".......",
 });
 
 con.connect(function (err) {
